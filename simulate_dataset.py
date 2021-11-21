e0db0f33edf11b4a78bedea41146a4b06d44f1d0
RyotaUshio <72342591+RyotaUshio@users.noreply.github.com>
2021-11-21 23:21:56 +0900

    fix

--- simulate_dataset.py
+++ simulate_dataset.py
@@ -10,7 +10,7 @@ import re
 import argparse
 import time
 
-_parse_fname_pattern = re.compile(r'(.+)_(\d{4})(_class\d)?\.(.+)')
+_parse_fname_pattern = re.compile(r'(.+)_(\d{4})?(_class\d)?\.(.+)')
 _parse_fname_i_class_pattern = re.compile(r'_class(\d+)')
 
 def parse_fname(fname):
@@ -58,9 +58,7 @@ def imread(fname):
       str(fname),
       mode=torchvision.io.ImageReadMode.RGB
     )
-
-def vidread(fname):
-  print()
+    
 
 class foreground_obj:
     _affine = torchvision.transforms.RandomAffine(
@@ -106,7 +104,7 @@ class foreground_obj:
         return torch.where(mask_rgb, image, background)
         
     def argument(self):
-        pass
+        """新しいオブジェクトを返すように！！"""
         
     def make_bbox(self):
         pass
@@ -152,43 +150,126 @@ class set_foreground_obj:
 
     def __iter__(self):
         return iter(self.objects)
-    
-    
-class background:
-    pass
 
+    @classmethod
+    def merge(cls, *args):
+        ret = []
+        for arg in args:
+            assert isinstance(arg, cls)
+            ret += arg.objects
+        return ret
+
+
+def synthesize(frame, objs, classes, prob, output_name):
+    rng = np.random.default_rng()
+    
+    """nut(fine)
+    nut(empty)
+    burr
+    burr+nut
+    """
+    n_obj = rng.normal(loc=6, scale=2) # ???
+    
+    for i in range(n_obj):
+        class_name = rng.choice(classes, p=prob)
+        objects = objs[class_name]
+        obj = rng.choice(objects)
+        obj = obj.argumented()
+        frame = background_argumentation(frame)
+        synthesized = obj.random_place(frame)
+        torchvision.utils.save_image(synthesized, output_name)
+        
+        
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('mask_dir', help='input directory where mask files named [image_name]_class[class_id].npy are placed')
     parser.add_argument('rep_dir', help='input directory where representative image files are placed. Note that a filename must have the format of [video name]_[%%M%%S] where %%M and %%S denote minute and second as decimal numbers, respectedly')
     parser.add_argument('video_dir', help='input directory where video files of chestnut fields are placed')
     parser.add_argument('background_dir', help='input directory where background video files are placed')
+    parser.add_argument('output_dir', help='output directory where generated dataset will be exported. Note that two directories will be made under the output_dir, namely "images" and "labels"')
     parser.add_argument('labels', help='labels file')
+    parser.add_argument('n_sample', help='number of samples of the dataset that will be generated synthetically', type=int)
+    parser.add_argument('-p', '--probability', help='probability that a foreground object is chosen from each class when synthesizving dataset. This must have the same length as number of classes. Defaults to [1/n_class, 1/n_class, ...]', nargs='*', type=float)
     parser.add_argument('-e', '--extension', help='extension(s) of image files', nargs='?', default='.png')
     args = parser.parse_args()
+    if not args.extension.startswith('.'):
+        args.extension = '.' + args.extension
     return args
 
 def main():
     args = parse_args()
     p_mask_dir = pathlib.Path(args.mask_dir)
     p_rep_dir = pathlib.Path(args.rep_dir)
+    p_video_dir = pathlib.Path(args.video_dir)
+    p_back_dir = pathlib.Path(args.background_dir)
     classes = get_classes(args.labels)
+    n_class = len(classes)
 
-    # fg: foreground images
-    # bg: background images
+    # class probabilities
+    if args.probability:
+        assert len(args.probability) == n_class
+    else:
+        args.probability = [1.0/n_class] * n_class
+    prob = np.array(args.probablity)
+    prob /= prob.sum()
 
-    i = 0
+    # stem = filename - suffix : corresponds to each video clip with foreground objects
+    stems = [parse_fname(p_video.name)['stem'] for p_video in p_video_dir.glob('*.mp4')]
+    obj_dict = {
+        stem: {class_name: [] for class_name in classes}
+        for stem in stems
+    }
+
+    # generate foreground objects and put them into obj_dict
     for p_mask in p_mask_dir.glob('*.npy'):
         p_image = get_image_path(p_mask=p_mask, p_rep_dir=p_rep_dir, extension=args.extension)
-        objs = set_foreground_obj(p_image, p_mask, classes) # a set of foreground objects contained in p_image
-      
-        for obj in objs:
-            obj.show(center=True)
-            plt.show()
-            plt.pause(1)
-            i += 1
-        if i > 5:
-          break
+        set_obj = set_foreground_obj(p_image, p_mask, classes) # a set of foreground objects contained in p_image
+        obj_dict[set_obj.stem][set_obj.class_name].extend(set_obj.objects)
+
+    # simulate datasets for each pair of (a background video, set of foregound objects in a video)
+    rng = np.random.default_rng()
+    p_back_generator = p_back_dir.glob('*.mp4')
+    n_back_video = len(list(p_back_generator))
+    n_obj_video = len(stems)
+    n_sample_per_pair = args.n_sample // (n_back_video * n_obj_video)
+    n_sample_generated = 0
+    
+    for p_video in p_back_generator:
+
+        # open background video
+        cap = cv2.VideoCapture(str(p_video))
+        if not cap.isOpened():
+            raise Exception(f'Cannot open file {p_video}')
+        n_frame = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+        
+        for stem in stems:
+            # randomly select a background frame from p_video
+            idx_frame = rng.choice(
+                range(n_frame),
+                size=n_sample_per_pair
+                replace=False
+            )
+            for i_frame in idx_frame:
+                cap.set(cv2.CAP_PROP_POS_FRAMES, i_frame)
+                ret, frame = cap.read()
+                assert ret, f'Could not read {i_frame}-th frame from the background video {p_video} with {n_frame} frames'
+                synthesize(
+                    frame=frame,
+                    objs=obj_dict[stem],
+                    classes=classes,
+                    prob=prob,
+                    output_name=os.path.join(args.output_dir, f'back_{p_video.stem}_rep_{stem}{args.extension}')
+                )
+
+
+    
+        
+        sec = cap.get(cv2.CAP_PROP_POS_MSEC) * 1000
+        
+        func(frame, sec, *args, **kwargs)
+    
+    cap.release()
+
         
 if __name__ == '__main__':
     main()
