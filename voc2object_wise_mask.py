7dfeeaaa54deaca98757c0352ed52201ede01289
RyotaUshio <72342591+RyotaUshio@users.noreply.github.com>
2021-11-21 11:21:50 +0900

    fix

--- voc2object_wise_mask.py
+++ voc2object_wise_mask.py
@@ -11,6 +11,10 @@ def main():
     parser.add_argument('labels', help='labels file')
     args = parser.parse_args()
 
+    # make output directory
+    pathlib.Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+
+    # read labels.txt and get class information
     classes = []
     with open(args.labels) as f:
         for line in f:
@@ -18,8 +22,6 @@ def main():
             if line not in ['__ignore__', '_background_']:
                 classes.append(line)
     n_class = len(classes)
-                
-    masks_each_class = {class_name: [] for class_name in classes}
     
     for path in pathlib.Path(args.input_dir).glob('*.npy'):
         labels = np.load(path)
@@ -27,13 +29,8 @@ def main():
             object_wise_labels = measure.label(labels == i_class + 1) # labels connected components
             object_id = np.unique(object_wise_labels)[1:] # ignore background = 0
             masks = (object_wise_labels == object_id.reshape(-1, 1, 1))
-            masks_each_class[class_name].append(masks)
-
-    for class_name in classes:
-        masks = np.concatenate(masks_each_class[class_name], axis=0)
-        pathlib.Path(args.output_dir).mkdir(parents=True, exist_ok=True)
-        output_filename = os.path.join(args.output_dir, class_name)
-        np.save(output_filename, masks)
+            output_filename = os.path.join(args.output_dir, f'{path.stem}_class{i_class+1}')
+            np.save(output_filename, masks)
             
 if __name__ == '__main__':
     main()
