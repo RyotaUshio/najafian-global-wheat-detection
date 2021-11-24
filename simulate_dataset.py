efadef14060d8c1efc536c6cbd05883dc7a591f9
RyotaUshio <gt0410ushi@g.ecc.u-tokyo.ac.jp>
2021-11-24 20:13:36 +0000

    almost finish except for data argmentation

--- simulate_dataset.py
+++ simulate_dataset.py
@@ -6,7 +6,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 import pathlib
-import os
 import re
 import argparse
 import dataclasses
@@ -59,9 +58,6 @@ def get_classes(p_labels):
                 classes.append(line)
     return classes
 
-def mask_3_channels(mask):
-    return torch.stack([mask] * 3)
-
 def tensorimage_to_numpy(tensor):
     """(C x H x W) to (H x W x C)
     """
@@ -108,11 +104,7 @@ def get_bbox(mask):
     
 @dataclasses.dataclass
 class foreground_obj:
-    _affine: ClassVar[Callable] = torchvision.transforms.RandomAffine(
-        degrees=180,    
-        translate=(0.4, 0.4)
-    )
-    _totensor: ClassVar[Callable] = torchvision.transforms.ToTensor()
+    _random_rotate: ClassVar[Callable] = torchvision.transforms.RandomRotation(degrees=180, expand=True, fill=0)
 
     image: torch.Tensor
     mask: torch.Tensor
@@ -123,21 +115,14 @@ class foreground_obj:
     i_class: int
     class_name: str
     extension: str
-    maks_rgb: torch.Tensor = dataclasses.field(init=False)
-    height: int = dataclasses.field(init=False)
-    width: int = dataclasses.field(init=False)
-    g: torch.Tensor = dataclasses.field(init=False)
-    centered_image: torch.Tensor = dataclasses.field(init=False)
-    centered_mask: torch.Tensor = dataclasses.field(init=False)
-    centered_mask_rgb: torch.Tensor = dataclasses.field(init=False)
-    x_min: int = dataclasses.field(init=False)
-    x_max: int = dataclasses.field(init=False)
-    y_min: int = dataclasses.field(init=False)
-    y_max: int = dataclasses.field(init=False)
+    image_height: int = dataclasses.field(init=False)
+    image_width: int = dataclasses.field(init=False)
+    bbox: dict = dataclasses.field(init=False)
+    image_cropped: torch.Tensor = dataclasses.field(init=False)
+    mask_cropped: torch.Tensor = dataclasses.field(init=False)
 
     def __post_init__(self):
-        self.mask_rgb = mask_3_channels(self.mask)
-        self.height, self.width = self.mask.shape
+        _, self.image_height, self.image_width = self.image.size()
         self.bbox = get_bbox(self.mask)
         top, bottom, left, right = self.bbox['top'], self.bbox['bottom'], self.bbox['left'], self.bbox['right']
         self.image_cropped = self.image[:, top:bottom, left:right]
@@ -154,15 +139,13 @@ class foreground_obj:
         if background.shape[-1] == 3:
             background = background.transpose((-1, -3, -2))
 
-        rotate = torchvision.transforms.RandomRotation(degrees=180, expand=True, fill=0)
-        rotated = rotate(torch.cat([self.image_cropped, torch.unsqueeze(self.mask_cropped, 0)]))
+        rotated = self._random_rotate(torch.cat([self.image_cropped, torch.unsqueeze(self.mask_cropped, 0)]))
         image_cropped, mask_cropped = torch.split(rotated, [3, 1], dim=0)
-        mask_cropped = torch.squeeze(mask_cropped, dim=0)
+        mask_cropped = torch.squeeze(mask_cropped, dim=0).to(torch.bool)
         _, h, w = image_cropped.size() # size of cropped region after rotation
         
-        _, H, W = self.image.size() # size of the original image
-        top = np.random.randint(0, H - h)
-        left = np.random.randint(0, W - w)
+        top = np.random.randint(0, self.image_height - h)
+        left = np.random.randint(0, self.image_width - w)
         background[:, top:top+h, left:left+w][:, mask_cropped] = image_cropped[:, mask_cropped]
         
         # bounding box info
@@ -175,15 +158,6 @@ class foreground_obj:
         
     def argumented(self):
         """新しいオブジェクトを返すように！！"""
-        
-    def show(self, center=False):
-        mask = self.centered_mask_rgb if center else self.mask_rgb
-        image = self.centered_image if center else self.image
-        masked_image = torch.where(mask, image, torch.tensor(255, dtype=torch.uint8))
-        masked_image = tensorimage_to_numpy(masked_image)
-        plt.imshow(masked_image)
-        if not center:
-          plt.scatter(*self.g, marker='x', color='r')
     
     @classmethod
     def from_npy(cls, image_path, mask_path, classes=None):
@@ -228,6 +202,7 @@ class yolo_label:
     self.bboxes = []
     self.image_width = image_width
     self.image_height = image_height
+    self.dicts =  []
 
   def add(self, i_class: int, bbox: dict):
     x_center = 0.5 * (bbox['left'] + bbox['right'])
@@ -250,6 +225,7 @@ class yolo_label:
         height=height
       )
     )
+    self.dicts.append(bbox)
 
   def save(self, fname):
     with open(fname, 'w') as f:
@@ -258,6 +234,11 @@ class yolo_label:
         row += '\n'
         f.write(row)
 
+  def to_tensor(self):
+      return torch.tensor([[bbox['left'], bbox['top'], bbox['right'], bbox['bottom']] for bbox in self.dicts])
+
+  def class_name_list(self, classes):
+      return [classes[bbox['i_class']] for bbox in self.bboxes]
 
 
 def synthesize(frame, objs, classes, prob, width, height):
@@ -284,33 +265,46 @@ def synthesize(frame, objs, classes, prob, width, height):
         
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('mask_dir', help='input directory where mask files named [image_name]_class[class_id].npy are placed')
-    parser.add_argument('rep_dir', help='input directory where representative image files are placed. Note that a filename must have the format of [video name]_[%%M%%S] where %%M and %%S denote minute and second as decimal numbers, respectedly')
-    parser.add_argument('video_dir', help='input directory where video files of chestnut fields are placed')
-    parser.add_argument('background_dir', help='input directory where background video files are placed')
-    parser.add_argument('output_dir', help='output directory where generated dataset will be exported. Note that two directories will be made under the output_dir, namely "images" and "labels"')
-    parser.add_argument('labels', help='labels file')
+    parser.add_argument('-r', '--root', help='root directory of the project (optional). if given, all other pathes are assumed to be relative to the root.', type=pathlib.Path)
+    parser.add_argument('mask_dir', help='input directory where mask files named [image_name]_class[class_id].npy are placed', type=pathlib.Path)
+    parser.add_argument('rep_dir', help='input directory where representative image files are placed. Note that a filename must have the format of [video name]_[%%M%%S] where %%M and %%S denote minute and second as decimal numbers, respectedly', type=pathlib.Path)
+    parser.add_argument('video_dir', help='input directory where video files of chestnut fields are placed', type=pathlib.Path)
+    parser.add_argument('background_dir', help='input directory where background video files are placed', type=pathlib.Path)
+    parser.add_argument('output_dir', help='output directory where generated dataset will be exported. Note that two directories will be made under the output_dir, namely "images" and "labels"', type=pathlib.Path)
+    parser.add_argument('labels', help='path to the labels file', type=pathlib.Path)
     parser.add_argument('n_sample', help='number of samples of the dataset that will be generated synthetically', type=int)
     parser.add_argument('-p', '--probability', help='probability that a foreground object is chosen from each class when synthesizving dataset. This must have the same length as number of classes. Defaults to [1/n_class, 1/n_class, ...]', nargs='*', type=float)
     parser.add_argument('-e', '--extension', help='extension(s) of image files', nargs='?', default='.png')
+    parser.add_argument('-v', '--verbose', help='if specified, display the progress and time remaining', action='store_true')
+    parser.add_argument('-b', '--bbox', help='if specified, images with calculated bounding boxes are also saved', action='store_true')
     args = parser.parse_args()
     if not args.extension.startswith('.'):
         args.extension = '.' + args.extension
+    if args.root is not None:
+        args_dict = vars(args)
+        for key in ['mask_dir', 'rep_dir', 'video_dir', 'background_dir', 'output_dir', 'labels']:
+            args_dict[key] = args.root / args_dict[key]
     return args
 
 if __name__ == '__main__':
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
     
     args = parse_args()
-    p_mask_dir = pathlib.Path(args.mask_dir)
-    p_rep_dir = pathlib.Path(args.rep_dir)
-    p_video_dir = pathlib.Path(args.video_dir)
-    p_back_dir = pathlib.Path(args.background_dir)
-    p_output_dir = pathlib.Path(args.output_dir)
+    
+    p_mask_dir = args.mask_dir
+    p_rep_dir = args.rep_dir
+    p_video_dir = args.video_dir
+    p_back_dir = args.background_dir
+    p_output_dir = args.output_dir
     p_output_images_dir = p_output_dir / 'images'
-    p_output_labels_dir = p_output_dir / 'labels'
+    p_output_labels_dir = p_output_dir / 'labels'                
     p_output_images_dir.mkdir(parents=True, exist_ok=True)
     p_output_labels_dir.mkdir(parents=True, exist_ok=True)
+
+    if args.bbox:
+        p_output_labeled_images_dir = p_output_dir / 'labeled_images'
+        p_output_labeled_images_dir.mkdir(exist_ok=True)
+    
     classes = get_classes(args.labels)
     n_class = len(classes)
 
@@ -335,10 +329,14 @@ if __name__ == '__main__':
     }
 
     # generate foreground objects and put them into obj_dict
+    if args.verbose:
+        print('constructing foreground objects...', end='')
     for p_mask in p_mask_dir.glob('*.npy'):
         p_image = get_image_path(p_mask=p_mask, p_rep_dir=p_rep_dir, extension=args.extension)
         objects, stem, class_name = foreground_obj.from_npy(p_image, p_mask, classes) # a list of foreground objects contained in p_image
         obj_dict[stem][class_name].extend(objects)
+    if args.verbose:
+        print('done')
 
     # simulate datasets for each pair of (a background video, set of foregound objects in a video)
     rng = np.random.default_rng()
@@ -346,52 +344,99 @@ if __name__ == '__main__':
     n_obj_video = len(stems)
     n_sample_per_pair = args.n_sample // (n_back_video * n_obj_video)
     n_sample_generated = 0
+
+    if args.verbose:
+        print('synthesizing dataset...')
     
-    for p_video in p_back_dir.glob('*.mp4'):
+    t0 = time.time()
+    counts = dict()
+    finished = False
 
-        # open background video
-        cap = cv2.VideoCapture(str(p_video))
-        if not cap.isOpened():
-            raise Exception(f'Cannot open file {p_video}')
-        n_frame = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-        n_frame = int(n_frame)
-        
-        for stem in rep_stems:
-            # randomly select a background frame from p_video
-            idx_frame = rng.choice(
-                range(n_frame),
-                size=n_sample_per_pair,
-                replace=False
-            )
-            for idx, i_frame in enumerate(idx_frame):
-                try:
-                    frame = read_frame(cap, i_frame)
-                except RuntimeError as e:
-                    print('Something went wrong while reading frames from a video:')
-                    print(e)
+    while True:
+        for p_video in p_back_dir.glob('*.mp4'):
 
-                width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-                height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+            # open background video
+            cap = cv2.VideoCapture(str(p_video))
+            if not cap.isOpened():
+                raise Exception(f'Cannot open file {p_video}')
+            n_frame = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+            n_frame = int(n_frame)
+            if p_video not in counts:
+                counts[p_video] = {}
+            
+            for stem in rep_stems:
 
-                frame, label = synthesize(
-                    frame=frame,
-                    objs=obj_dict[stem],
-                    classes=classes,
-                    prob=prob,
-                    width=width,
-                    height=height
-                )
+                if stem not in counts[p_video]:
+                    counts[p_video][stem] = 0
+                else:
+                    counts[p_video][stem] += 1
+                
+                for _ in range(n_sample_per_pair):
+                    # randomly select a background frame from p_video
+                    i_frame = rng.choice(n_frame)
+                    try:
+                        frame = read_frame(cap, i_frame)
+                    except RuntimeError as e:
+                        print('Something went wrong while reading frames from a video:')
+                        print(e)
 
-                # save as files
-                output_stem = f'back_{p_video.stem}_rep_{stem}_synthesized_{idx}'
-                output_image_name = os.path.join(p_output_images_dir, output_stem + args.extension)
-                output_label_name = os.path.join(p_output_labels_dir, output_stem + '.txt')
-                save_image(frame, output_image_name)
-                label.save(output_label_name)
+                    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+                    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-                n_sample_generated += 1
-                if n_sample_generated % 10 == 0:
-                    print(f'{n_sample_generated}/{args.n_sample} = {n_sample_generated/args.n_sample*100}% completed')
+                    frame, label = synthesize(
+                        frame=frame,
+                        objs=obj_dict[stem],
+                        classes=classes,
+                        prob=prob,
+                        width=width,
+                        height=height
+                    )
 
-        cap.release()
+                    # save as files
+                    output_stem = f'back_{p_video.stem}_rep_{stem}_synthesized_{counts[p_video][stem]}'
+                    output_image_name = p_output_images_dir / (output_stem + args.extension)
+                    output_label_name = p_output_labels_dir / (output_stem + '.txt')
+                    save_image(frame, output_image_name)
+                    label.save(output_label_name)
+
+                    if args.bbox:
+                        output_labeled_image_name = p_output_labeled_images_dir / (output_stem + args.extension)
+                        save_image(
+                            torchvision.utils.draw_bounding_boxes(
+                                image=frame,
+                                boxes=label.to_tensor(),
+                                labels=label.class_name_list(classes)
+                            ),
+                            output_labeled_image_name,
+                        )
+
+                    n_sample_generated += 1
+                    if n_sample_generated >= args.n_sample:
+                        finished = True
+                        break
+
+                    if args.verbose:
+                        time_elapsed = time.time() - t0
+                        velo = n_sample_generated / time_elapsed
+                        time_remaining = (args.n_sample - n_sample_generated) / velo
+                        print(
+                            f'  {n_sample_generated}/{args.n_sample} = {n_sample_generated/args.n_sample*100:.1f}% completed. '
+                            f'About {time.strftime("%Hh %Mmin", time.gmtime(time_remaining + 30))} left.\r',
+                            end=''
+                        )
+
+                    if finished:
+                        break
+
+                if finished:
+                    break
+
+            if finished:
+                break
+
+        if finished:
+            break
+
+    if args.verbose:
+        print('\ndone')
 
