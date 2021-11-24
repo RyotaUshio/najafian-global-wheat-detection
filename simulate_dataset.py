58d86e21fb93f488782b5310ff5108822a354b65
RyotaUshio <gt0410ushi@g.ecc.u-tokyo.ac.jp>
2021-11-24 17:20:15 +0000

    remove surplus computation

--- simulate_dataset.py
+++ simulate_dataset.py
@@ -9,6 +9,8 @@ import pathlib
 import os
 import re
 import argparse
+import dataclasses
+from typing import ClassVar, Callable
 import time
 from collections import OrderedDict
 
@@ -73,45 +75,76 @@ def imread(fname):
       mode=torchvision.io.ImageReadMode.RGB
     )
 
-def save_image(tensor, fname):
-  if tensor.dtype == torch.uint8:
-    tensor = tensor.to(float)
-    tensor /= 255.0 
-  torchvision.utils.save_image(tensor, fname)
+read_image = imread
+
+def read_frame(cap, i_frame):
+    cap.set(cv2.CAP_PROP_POS_FRAMES, i_frame)
+    opened, frame = cap.read()
+    if not opened:
+        raise RuntimeError('Could not read {i_frame}-th frame from the background video {p_video} with {n_frame} frames')
+    frame = frame[:, :, [2, 1, 0]] # OpenCV reads image in (B, G, R(, A)) order
+    frame = frame.transpose((2, 0, 1)) # (H, W, C) -> (C, H, W)
+    frame = torch.as_tensor(frame)
+    return frame
     
 
+def save_image(tensor, fname):
+    if isinstance(tensor, torch.ByteTensor):
+        tensor = tensor.float()
+        tensor /= 255.0
+    torchvision.utils.save_image(tensor, fname)
+  
+def get_bbox(mask):
+    if mask.ndim <= 2:
+        mask = torch.unsqueeze(mask, 0)
+    [[left, top, right, bottom]] = torchvision.ops.masks_to_boxes(mask).to(int)
+    return dict(
+        left=left.item(), 
+        right=right.item(), 
+        top=top.item(), 
+        bottom=bottom.item()
+    )
+
+    
+@dataclasses.dataclass
 class foreground_obj:
-    _affine = torchvision.transforms.RandomAffine(
-        degrees=180,
+    _affine: ClassVar[Callable] = torchvision.transforms.RandomAffine(
+        degrees=180,    
         translate=(0.4, 0.4)
     )
-    _totensor = torchvision.transforms.ToTensor()
-    
-    def __init__(self, image, mask):
-        self.image = image
-        self.mask = mask
+    _totensor: ClassVar[Callable] = torchvision.transforms.ToTensor()
+
+    image: torch.Tensor
+    mask: torch.Tensor
+    image_path: pathlib.Path
+    mask_path: pathlib.Path
+    stem: str
+    time: time.struct_time
+    i_class: int
+    class_name: str
+    extension: str
+    maks_rgb: torch.Tensor = dataclasses.field(init=False)
+    height: int = dataclasses.field(init=False)
+    width: int = dataclasses.field(init=False)
+    g: torch.Tensor = dataclasses.field(init=False)
+    centered_image: torch.Tensor = dataclasses.field(init=False)
+    centered_mask: torch.Tensor = dataclasses.field(init=False)
+    centered_mask_rgb: torch.Tensor = dataclasses.field(init=False)
+    x_min: int = dataclasses.field(init=False)
+    x_max: int = dataclasses.field(init=False)
+    y_min: int = dataclasses.field(init=False)
+    y_max: int = dataclasses.field(init=False)
+
+    def __post_init__(self):
         self.mask_rgb = mask_3_channels(self.mask)
         self.height, self.width = self.mask.shape
+        self.bbox = get_bbox(self.mask)
+        top, bottom, left, right = self.bbox['top'], self.bbox['bottom'], self.bbox['left'], self.bbox['right']
+        self.image_cropped = self.image[:, top:bottom, left:right]
+        self.mask_cropped = self.mask[top:bottom, left:right]
         
-        # get the center of gravity
-        indices = np.indices(self.mask.shape)[:, self.mask]
-        g_x = np.mean(indices[1])
-        g_y = np.mean(indices[0])
-        self.g = torch.tensor([g_x, g_y])
-        
-        self.centered_image = self.centerize(self.image)
-        self.centered_mask = self.centerize(self.mask)
-        self.centered_mask_rgb = mask_3_channels(self.centered_mask)
-
-    def centerize(self, img):
-        # move to the center
-        center = torch.tensor([self.width, self.height]) * 0.5
-        delta = center - self.g
-        delta_x, delta_y = delta.to(int).tolist()
-        return torch.roll(img, shifts=(delta_x, delta_y), dims=(-1, -2))
-
     def random_place(self, background):
-        """Randomly place the object on the given background image.
+        """Randomly place the object on the given background image. This is an in-place operation.
         
         Parameters
         ----------
@@ -120,30 +153,29 @@ class foreground_obj:
         """
         if background.shape[-1] == 3:
             background = background.transpose((-1, -3, -2))
-        background = torch.as_tensor(background)
-        image, mask_rgb = self._affine(
-          torch.stack(
-            [self.centered_image, self.centered_mask_rgb]
-          )
-        )
-        result = torch.where(mask_rgb, image, background)
+
+        rotate = torchvision.transforms.RandomRotation(degrees=180, expand=True, fill=0)
+        rotated = rotate(torch.cat([self.image_cropped, torch.unsqueeze(self.mask_cropped, 0)]))
+        image_cropped, mask_cropped = torch.split(rotated, [3, 1], dim=0)
+        mask_cropped = torch.squeeze(mask_cropped, dim=0)
+        _, h, w = image_cropped.size() # size of cropped region after rotation
+        
+        _, H, W = self.image.size() # size of the original image
+        top = np.random.randint(0, H - h)
+        left = np.random.randint(0, W - w)
+        background[:, top:top+h, left:left+w][:, mask_cropped] = image_cropped[:, mask_cropped]
+        
         # bounding box info
-        [[left, top, right, bottom]] = torchvision.ops.masks_to_boxes(mask_rgb[[0]])
-        bbox = dict(
-          left=left.item(), 
-          right=right.item(), 
-          top=top.item(), 
-          bottom=bottom.item()
-        )
-        return result, bbox
+        bbox = get_bbox(mask_cropped)
+        bbox['top'] += top
+        bbox['bottom'] += top
+        bbox['left'] += left
+        bbox['right'] += left
+        return bbox
         
     def argumented(self):
         """新しいオブジェクトを返すように！！"""
         
-        
-    def make_bbox(self):
-        pass
-
     def show(self, center=False):
         mask = self.centered_mask_rgb if center else self.mask_rgb
         image = self.centered_image if center else self.image
@@ -153,46 +185,42 @@ class foreground_obj:
         if not center:
           plt.scatter(*self.g, marker='x', color='r')
     
-class set_foreground_obj:
-    def __init__(self, image_path, mask_path, classes=None):
-        self.image_path = image_path
-        self.mask_path = mask_path
-
-        parsed = parse_fname(self.mask_path.name)
+    @classmethod
+    def from_npy(cls, image_path, mask_path, classes=None):
+        """generate a list of foreground objects from .npy file
+        """
+        image_path = pathlib.Path(image_path)
+        mask_path = pathlib.Path(mask_path)
+        parsed = parse_fname(mask_path.name)
         
-        self.stem = parsed['stem'] # filename without extensions
-        self.time = time.strptime(parsed['time'], '%M%S') # time elapsed since the start of the vid
-        self.i_class = parsed['i_class']
-        self.class_name = None
+        stem = parsed['stem'] # filename without extensions
+        time_elapsed = time.strptime(parsed['time'], '%M%S') # time elapsed since the start of the vid
+        i_class = parsed['i_class']
+        class_name = None
         if classes is not None:
-            self.class_name = classes[self.i_class - 1] # i_class is 1-origin
-        self.extension = parsed['extension'] # extension of image file e.g. 'png', 'jpeg', etc.
+            class_name = classes[i_class - 1] # i_class is 1-origin
+        extension = parsed['extension'] # extension of image file e.g. 'png', 'jpeg', etc.
 
         # read mask & image files
-        self.masks = torch.as_tensor(np.load(self.mask_path))
-        self.image = imread(self.image_path)
+        masks = torch.as_tensor(np.load(mask_path))
+        image = imread(image_path)
 
-        self.objects = []
-        for mask in self.masks:
-            fg = foreground_obj(image=self.image, mask=mask)
-            self.objects.append(fg)
+        objects = []
+        for mask in masks:
+            fg = foreground_obj(
+                image=image, 
+                mask=mask,
+                image_path=image_path,
+                mask_path=mask_path,
+                stem=stem,
+                time=time_elapsed,
+                i_class = i_class,
+                class_name=class_name,
+                extension = extension
+            )               
+            objects.append(fg)
 
-    def __len__(self):
-        return len(self.objects)
-
-    def __getitem__(self, key):
-        return self.objects[key]
-
-    def __iter__(self):
-        return iter(self.objects)
-
-    @classmethod
-    def merge(cls, *args):
-        ret = []
-        for arg in args:
-            assert isinstance(arg, cls)
-            ret += arg.objects
-        return ret
+        return objects, stem, class_name
 
 
 class yolo_label:
@@ -233,6 +261,8 @@ class yolo_label:
 
 
 def synthesize(frame, objs, classes, prob, width, height):
+    """randomly place foreground objects onto the given frame in-place
+    """
     rng = np.random.default_rng()
     n_obj = int(rng.normal(loc=6, scale=1.5)) # 謎のヒューリティクス
     n_class = len(classes)
@@ -248,7 +278,7 @@ def synthesize(frame, objs, classes, prob, width, height):
             break
         obj = rng.choice(objects)
         # obj = obj.argumented() 
-        frame, bbox = obj.random_place(frame)
+        bbox = obj.random_place(frame)
         label.add(i_class, bbox)
     return frame, label
         
@@ -307,8 +337,8 @@ if __name__ == '__main__':
     # generate foreground objects and put them into obj_dict
     for p_mask in p_mask_dir.glob('*.npy'):
         p_image = get_image_path(p_mask=p_mask, p_rep_dir=p_rep_dir, extension=args.extension)
-        set_obj = set_foreground_obj(p_image, p_mask, classes) # a set of foreground objects contained in p_image
-        obj_dict[set_obj.stem][set_obj.class_name].extend(set_obj.objects)
+        objects, stem, class_name = foreground_obj.from_npy(p_image, p_mask, classes) # a list of foreground objects contained in p_image
+        obj_dict[stem][class_name].extend(objects)
 
     # simulate datasets for each pair of (a background video, set of foregound objects in a video)
     rng = np.random.default_rng()
@@ -334,12 +364,11 @@ if __name__ == '__main__':
                 replace=False
             )
             for idx, i_frame in enumerate(idx_frame):
-                cap.set(cv2.CAP_PROP_POS_FRAMES, i_frame)
-                ret, frame = cap.read()
-                assert ret, f'Could not read {i_frame}-th frame from the background video {p_video} with {n_frame} frames'
-                frame = frame[:, :, [2, 1, 0]] # OpenCV reads image in (B, G, R(, A)) order
-                
-                
+                try:
+                    frame = read_frame(cap, i_frame)
+                except RuntimeError as e:
+                    print('Something went wrong while reading frames from a video:')
+                    print(e)
 
                 width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
                 height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
@@ -361,7 +390,7 @@ if __name__ == '__main__':
                 label.save(output_label_name)
 
                 n_sample_generated += 1
-                if n_sample_generated % 100 == 0:
+                if n_sample_generated % 10 == 0:
                     print(f'{n_sample_generated}/{args.n_sample} = {n_sample_generated/args.n_sample*100}% completed')
 
         cap.release()
